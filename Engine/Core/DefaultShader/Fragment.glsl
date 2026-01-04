b6a22b8e72b37bcb12206ae580c3d69ea6c84ac2
MikeJohnnyP <tahoangphuc1901@gmail.com>
2026-01-04 18:41:19 +0700

    Texture work for openGL 3.3

--- Engine/Core/DefaultShader/Fragment.glsl
+++ Engine/Core/DefaultShader/Fragment.glsl
@@ -8,6 +8,7 @@ in vec2 textureNormal;
 out vec4 FragColor;
 
 void main() {
-//    FragColor = mix(texture(texture1, textureNormal), texture(texture2, textureNormal), 0.8);
-    FragColor =  u_Color;
+   // FragColor = mix(texture(texture1, textureNormal), texture(texture2, textureNormal), 0.8);
+    // FragColor =  u_Color;
+   FragColor = texture(u_Texture, textureNormal) * u_Color;
 }
--- Sandbox/Layer2D.h
+++ Sandbox/Layer2D.h
@@ -15,8 +15,7 @@ class Layer2D : public Gnote::Layer
     }
     virtual void OnAttach() override
     {
-        // m_Texture =
-        // Gnote::Texture2D::Create("./build/bin/Assets/awesomeface.png");
+        m_Texture = Gnote::Texture2D::Create("./build/bin/Assets/awesomeface.png");
     }
     virtual void OnDettach() override
     {
@@ -37,12 +36,13 @@ class Layer2D : public Gnote::Layer
             }
         }
 
-        // for (auto i = 0; i < 10; i++) {
-        //   for (auto y = 0; y < 10; y++) {
-        //     Gnote::Renderer2D::DrawQuad(glm::vec2(0.22f * i, 0.11f * y),
-        //                                 glm::vec2(0.1f), m_Texture);
-        //   }
-        // }
+        for (auto i = 0; i < 10; i++)
+        {
+            for (auto y = 0; y < 10; y++)
+            {
+                Gnote::Renderer2D::DrawQuad(glm::vec2(0.22f * i, 0.11f * y), glm::vec2(0.1f), m_Texture);
+            }
+        }
 
         Gnote::Renderer2D::EndScene();
     }
